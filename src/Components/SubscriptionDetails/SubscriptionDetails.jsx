29b67017c45589d64c607b9b02a78d276f3dac79
Kevin Newland <kevin.newland@yahoo.com>
2025-03-12 17:27:58 -0500

    add sorting for subscription lists

--- src/Components/SubscriptionDetails/SubscriptionDetails.jsx
+++ src/Components/SubscriptionDetails/SubscriptionDetails.jsx
@@ -11,8 +11,8 @@ function SubscriptionDetails( {specificSubscription} ) {
     console.log("Updated canceled subscriptions:", canceledSubscriptions);
   }, [canceledSubscriptions]);
 
-  function cancelCustomerSubscription(id) {
-    fetch(`http://localhost:3000/api/v1/customer_subscriptions/${id}`, {
+  function cancelCustomerSubscription(customerId,  subscriptionId) {
+    fetch(`http://localhost:3000/api/v1/customer_subscriptions/${customerId}/${subscriptionId}`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
@@ -21,7 +21,7 @@ function SubscriptionDetails( {specificSubscription} ) {
     .then(response => response.json())
     .then(data => {
       console.log("Fetched cancel Data:", data)
-      setCanceledSubscriptions(prev => [...prev, id]);
+      setCanceledSubscriptions(prev => [...prev, `${customerId}-${subscriptionId}`]);
   })
     .catch(error => console.log(error.message))
   }
@@ -30,44 +30,49 @@ function SubscriptionDetails( {specificSubscription} ) {
   return (
     <div className='SubscriptionDetails'>
       <div className='details-container'>
-      <h2>{subscription.attributes.title}</h2>
-      <p>Price: {subscription.attributes.price}</p>
-      <p>Frequency: {subscription.attributes.frequency}</p>
+        <h2>{subscription.attributes.title}</h2>
+        <p>Price: {subscription.attributes.price}</p>
+        <p>Frequency: {subscription.attributes.frequency}</p>
 
-      <h3>Teas</h3>
-      <ul>
-        {included.filter(item => item.type === "tea")
-        .map(tea => (
-          <li >
-            <p>Title: {tea.attributes.title}</p> 
-            <p>Description: {tea.attributes.description}</p>
-            <p>Temperature(in F*): {tea.attributes.temperature}</p>
-            <p>Brew Time(in Minutes): {tea.attributes.brew_time}</p>
-          </li>
-        ))}
-      </ul>
+        <h3>Teas</h3>
+        <ul>
+          {included.filter(item => item.type === "tea")
+          .map(tea => (
+            <li>
+              <p>Title: {tea.attributes.title}</p> 
+              <p>Description: {tea.attributes.description}</p>
+              <p>Temperature(in F*): {tea.attributes.temperature}</p>
+              <p>Brew Time(in Minutes): {tea.attributes.brew_time}</p>
+            </li>
+          ))}
+        </ul>
       </div>
+
       <h3>Customers that are Subscribed</h3>
       <ul>
-        {included.filter(item => item.type === "customer")
-        .map(customer => (
-          <li >
-            <p>First Name: {customer.attributes.first_name}</p> 
-            <p>Last Name: {customer.attributes.last_name}</p>
-            <p>Email: {customer.attributes.email}</p>
-            <p>Adress: {customer.attributes.address}</p>
-            {canceledSubscriptions.includes(customer.attributes.customer_subscription_id) ? (
-                  <span>Canceled!</span>
-                ) : (
-                  <button onClick={() => cancelCustomerSubscription(customer.attributes.customer_subscription_id)}>
-                    Cancel Subscription
-                  </button>
-                )}
-          </li>
-        ))}
+        {included.filter(item => item.type === "customer").map(customer => {
+          const canceled = `${customer.id}-${subscription.id}`; 
+
+          return (
+            <li>
+              <p>First Name: {customer.attributes.first_name}</p> 
+              <p>Last Name: {customer.attributes.last_name}</p>
+              <p>Email: {customer.attributes.email}</p>
+              <p>Address: {customer.attributes.address}</p>
+
+              {canceledSubscriptions.includes(canceled) ? (
+                <span className="canceled-text">Canceled!</span>
+              ) : (
+                <button onClick={() => cancelCustomerSubscription(customer.id, subscription.id)}>
+                  Cancel Subscription
+                </button>
+              )}
+            </li>
+          );
+        })}
       </ul>
     </div>
-  )
+  );
 }
 
 export default SubscriptionDetails;
